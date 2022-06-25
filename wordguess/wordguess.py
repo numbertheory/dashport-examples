ef71aad034781741c5bfa4bc72b6bf82a2322e27
JP Etcheber <jetcheber@gmail.com>
2022-06-25 11:48:11 -0700

    working with dashport 0.7.1

--- wordguess/wordguess.py
+++ wordguess/wordguess.py
@@ -3,9 +3,12 @@
 from dashport.dash import Dashport
 from dashport.run import wrap
 
+def exit_program(app):
+    exit(0)
+
 def dashport(stdscr):
     app = Dashport(stdscr)
-    app.add_control("q", quit, case_sensitive=False)
+    app.add_control("q", exit_program, case_sensitive=False)
     while True:
         app.refresh()
 
