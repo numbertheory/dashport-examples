b4d4a1edf6aed7ae64ca267e82892bd86316b85a
JP Etcheber <jetcheber@gmail.com>
2022-06-25 11:02:44 -0700

    example: word guessing game

--- wordguess/wordguess.py
+++ wordguess/wordguess.py
@@ -1,2 +1,14 @@
-#! /usr/env python3
+#! /usr/bin/env python3
 
+from dashport.dash import Dashport
+from dashport.run import wrap
+
+def dashport(stdscr):
+    app = Dashport(stdscr)
+    app.add_control("q", quit, case_sensitive=False)
+    while True:
+        app.refresh()
+
+
+if __name__ == '__main__':
+    wrap(dashport)
