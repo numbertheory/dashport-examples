233d2ab9cd67998e30dac78a15bc29e49436f4d7
JP Etcheber <jetcheber@gmail.com>
2022-06-25 21:21:47 -0700

    wip: figuring out key presses

--- wordguess/wordguess.py
+++ wordguess/wordguess.py
@@ -4,6 +4,7 @@ from dashport.dash import Dashport
 from dashport.run import wrap
 import random
 import argparse
+import curses
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--cheat', action='store_true', help='Show answer when program starts. For debugging. ;)')
@@ -14,13 +15,13 @@ if not args.cheat:
 else:
     cheat_mode = True
 
-def show_guess_grid(app):
+def show_guess_grid(app, **kwargs):
+    key_pressed = kwargs.get("key_pressed", " ")
     guesses = ["first", "second", "third", "fourth", "fifth", "sixth"]
     grid_row = 5
     for guess in guesses:
         for i in range(0, 5):
             app.panels[f"{guess}_guess_{str(i)}"] = app.panel(height=5, width=9, y=grid_row, x=20+(i*9), border=True)
-            app.print(" ", x=1, y=1, panel="first_guess_0.0")
         grid_row += 5
 
 def show_keyboard(app):
@@ -51,7 +52,17 @@ def show_keyboard(app):
                    x=pos_x, y=44, height=3, width=3, h_align="center")
         pos_x += 4
 
+def handle_guess(app):
+    app.key_pressed = app.screen.getkey().upper()
+    show_guess_grid(app, key_pressed=app.key_pressed)
 
+
+def load_controls(app):
+    letters = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+               'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
+               'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+    for key_control in letters:
+        app.add_control(key_control, handle_guess, case_sensitive=False)
 
 
 
@@ -66,13 +77,16 @@ def pick_word():
 def dashport(stdscr):
     app = Dashport(stdscr)
     app.layout("single_panel", border=False)
-    app.add_control("q", exit_program, case_sensitive=False)
+    app.add_control("KEY_F(1)", exit_program, case_sensitive=False)
     app.picked_word = pick_word()
     app.letter_list = list(app.picked_word.strip())
     if cheat_mode:
-        app.print(f"Cheat Mode: {app.picked_word}", panel="layout.0")
+        app.print(f"Cheat Mode: {app.picked_word.strip()} | Press F1 to quit", panel="layout.0")
+    else:
+        app.print(f"Press F1 to quit", panel="layout.0")
     show_guess_grid(app)
     show_keyboard(app)
+    load_controls(app)
     while True:
         app.refresh()
 
